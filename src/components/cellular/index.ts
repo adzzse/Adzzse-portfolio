19194315f1a77f32c6486edd3089462e4ca41b1a
Adzzse <noik0vshack@gmail.com>
2025-11-22 00:43:41 +0700

    nuke

--- src/components/cellular/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as CellularAutomaton } from './CellularAutomaton';
-export { default as GameOfLife } from './GameOfLife';
-export { default as Highlife } from './Highlife';
--- src/hooks/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './useCanvas';
-export * from './useGridConfig';
